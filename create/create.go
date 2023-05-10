24e6316800383a830e0aa14f61ea1434d56d1308
Travis Harmon <harmtrav@gmail.com>
2023-05-10 17:59:24 -0500

    Attempt to run go get after create command

--- create/create.go
+++ create/create.go
@@ -2,6 +2,7 @@ package create
 
 import (
 	"fmt"
+	"os"
 	"path"
 
 	"github.com/fatih/color"
@@ -38,11 +39,24 @@ func Create(name string) error {
 	if err := utils.WriteFile(path.Join(name, ".gitignore"), files.GitIgnore); err != nil {
 		return fail(err)
 	}
+	goGetSuccess := goGet(name)
 	utils.ClearTerminal()
 	color.Green("Successfully created %s!\n\n", name)
 	fmt.Printf("To get started, run the following commands:\n\n")
 	color.Blue("\tcd %s\n", name)
-	color.Blue("\tgo get main/src\n")
+	if !goGetSuccess {
+		color.Blue("\tgo get main/src\n")
+	}
 	color.Blue("\tgouix serve\n\n")
 	return nil
+}
+
+func goGet(name string) bool {
+	if err := os.Chdir(name); err != nil {
+		return false
+	}
+	if err := utils.Command("go", "get", "main/src"); err != nil {
+		return false
+	}
+	return true
 }
